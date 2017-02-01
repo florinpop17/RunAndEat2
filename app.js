1da05bd2d92bf04628208d86b35122cd15199afe
Florin Pop <popflorin1705@yahoo.com>
2017-02-01 23:37:23 +0200

    removed libs

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const server = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const io = require('socket.io').listen(server);
 const uuid = require('uuid/v4');
-const User = require('./libs/user');
-const Power = require('./libs/power');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,6 +13,7 @@ let powers = [];
 let maxNrOfPowers = 40;
 let timeOnNewPower = 700;
 let tickTime = 33; // 1000/30 => 30 frames/second
+let colors = ['#ecf0f1', '#3498db', '#2ecc71', '#ff2020'];
 
 let canvasWidth = 800;
 let canvasHeight = 800;
@@ -77,3 +76,28 @@ function addPower(){
         powers.push(new Power(uuid(), Math.random() * 800, Math.random() * 800, Math.floor(Math.random() * 4) + 1));
     }
 }
+
+class User {
+    constructor(_id, _name, _x, _y, _r, _speed, _col, _show){
+        this.id = _id;
+        this.name = _name;
+        this.x = _x;
+        this.y = _y;
+        this.r = _r;
+        this.speed = _speed;
+        this.col = _col;
+        this.show = _show;
+    }
+}
+
+class Power {
+    constructor(_id, _x, _y, _val){
+        this.id = _id;
+        this.x = _x;
+        this.y = _y;
+        this.r = 7;
+        this.val = _val;
+        this.col = colors[_val - 1];
+    }
+}
+
--- libs/user.js
+++ libs/user.js
@@ -1,14 +1,3 @@
-class User {
-    constructor(_id, _name, _x, _y, _r, _speed, _col, _show){
-        this.id = _id;
-        this.name = _name;
-        this.x = _x;
-        this.y = _y;
-        this.r = _r;
-        this.speed = _speed;
-        this.col = _col;
-        this.show = _show;
-    }
-}
+
 
 module.exports = User;
