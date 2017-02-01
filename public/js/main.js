91d0ee614a220a4127d1e4007d61d1ba9d6f00aa
Florin Pop <popflorin1705@yahoo.com>
2017-02-01 20:34:04 +0200

    createNewUser on client side

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 var socket;
+var user;
 var users = [];
 var powers = [];
-var name = "HiddenUser";
+var _name = "HiddenUser";
 
 function preload() {
     socket = io.connect();
-    socket.emit("start", name);
+    user = createNewUser(_name);
+    socket.emit("start", user);
 }
 
 function setup() {
@@ -20,4 +22,22 @@ function setup() {
 function draw() {
     background(0);
     
+}
+
+function createNewUser(_name) {
+    var name = _name;
+    var x = random(0, width);
+    var y = random(0, height);
+    var r = 20;
+    var speed = 5;
+    var col = color(random(255), random(255), random(255));
+    
+    return {
+        name: name,
+        x: x,
+        y: y,
+        r: r,
+        speed: speed,
+        col: col
+    }
 }
